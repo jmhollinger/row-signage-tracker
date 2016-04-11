51409bc89e6c4c74f15eed488a111a5b4192b27d
Jonathan Hollinger <jhollinger@lexucg.local>
2016-04-11 16:36:12 -0400

    Add Point Capture and Image Upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+//Firebase
+var db = new Firebase("https://lex-row-signs.firebaseio.com/signReports")
+
+function save (){
+ db.push({
+  "submissionTimestamp" : moment().format(), 
+  "datetime" : $("#datetime").val(),
+  "inspector" : $("#inspector").val(),
+  "name" : $("#name").val(),
+  "phone" : $("#phone").val(),
+  "website" : $("#URL").val(),
+  "email" : $("#email").val(),
+  "coordinates" : {"lat": $("#lat").val() ,"lng": $("#lng").val()},
+  "photo" : $("#photoData").val()
+})
+
+//window.location.pathname = "/row-signage-tracker/";
+
+}
+
+function previewFile() {
+  var preview = document.getElementById('preview')
+  var file    = document.getElementById('picture').files[0]
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    $("#photoData").val(reader.result)
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
+
 //Date & Time Picker
 $(function () {
                 $('#datetimepicker1').datetimepicker({
@@ -38,6 +72,9 @@ function initMap(){
   //Fill up the map-canvas.
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+
+  var geocoder = new google.maps.Geocoder;
+
   //Set default map bounds, expressed as southwest and northeast points.
   var defaultBounds = new google.maps.LatLngBounds(
       new google.maps.LatLng(37.921971, -84.663139),
@@ -51,6 +88,10 @@ var marker = new google.maps.Marker({
     draggable: true
   });
 
+google.maps.event.addListener(marker, 'dragend', function(evt){
+    $('#lat').val(evt.latLng.lat()); $('#lng').val(evt.latLng.lng());
+});
+
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
@@ -58,7 +99,7 @@ var marker = new google.maps.Marker({
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
+            $('#lat').val(pos.lat); $('#lng').val(pos.lng);
             marker.setPosition(pos);
             map.setCenter(pos);
             map.setZoom(18);
@@ -69,5 +110,6 @@ var marker = new google.maps.Marker({
           // Browser doesn't support Geolocation
           handleLocationError(false, marker, map.getCenter());
         }
+
 
   }
