cf556f0fe00d8ddef54f381e95dec1a26529160f
Jonathan Hollinger <jmholl5@gmail.com>
2016-05-13 08:59:54 -0400

    Create App Scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rowSignsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rowSignsApp
+ */
+angular.module('rowSignsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
